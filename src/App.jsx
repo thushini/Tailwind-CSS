379e7d1f5c1bcefa0775f9cfe64c6c2a34f7871a
thushini <thushini767@gmail.com>
2024-09-01 02:47:19 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <section className='h-screen bg-twitter-blue flex items-center justify-center
+      m-12 text-center text-3xl p-12 md:p-14 lg:pd-16 xl:p-20'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nihil ad, veniam quo quibusdam sapiente doloribus nobis ab nemo explicabo dolor nulla minima repudiandae magni consequatur temporibus magnam beatae quod!</p>
+      </section>
+    </div>
+  )
+}
+
+export default App
